46129a2a545d1f0567967a16793afb7918ebdefc
Ian Foster <idfoster@ucsd.edu>
2013-04-12 15:34:32 -0700

    removed comment

--- labrat/serverEnum.sh
+++ labrat/serverEnum.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 for i in $(seq 200 400);
 do
   #echo Checking ieng6-$i
   result=$(host ieng6-$i | grep "address")
-  #result=$(host ieng6-$i | grep "not found")
   if [ -n "$result" ];
   then
     echo ieng6-$i
