7bc6516220fd719c9825f1d4833c7e210559e4dd
Devang Sampat <devangs27@gmail.com>
2013-04-13 06:11:07 -0700

    Adding js stuff

--- server/static/tutor.js
+++ server/static/tutor.js
@@ -1,7 +1,20 @@
 function getTutors(classid, callback){ 
    $.get("/tutors/"+classid+".json", {success: callback});
 }
-
+$(function(){
 getTutors(cid,function(data){ 
-	$("#tutors").html= 
+	var output = '<div class="boardName bigFont">the tutors</div>'; 
+	for (var i = 0; i < data.length; i++){
+		output += '<div class="tutor">';
+		output += '<img src =' + data[i].profPicUrl + '/>';
+		output += '<div class="details">';
+		output += '<div class="bigFont">' + data[i].name + '</div>';
+		output += '<div class="smallFont">until '+ data[i].endHours + '</div>';
+		output += '</div>';
+		output += '</div>';
+	}
+
+	$("#tutors").html= output;				
+					
  })
+});
