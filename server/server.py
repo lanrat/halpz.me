a329c1ff7499030fb0938bf63403b7a1ceed5434
Devin <devin@devin-VirtualBox.(none)>
2013-04-12 21:58:20 -0700

    added all routes

--- server/server.py
+++ server/server.py
@@ -8,33 +8,65 @@ r = model.RedisModel()
 #default
 @app.route('/')
 def index():
+    #FOR STUDENTS
+    #select class they need help for(they will just type in the number)
+            #goto helpwith
+    #show everyone on the board right now, the tutors tutoring their class right now, maybe an ETA
+    #if they not asked for help
+            #show an ask for help button with autofill name and location field based on Ian's API
+    #else
+            #show their place in the queue, ie 4th on the list(should update in real time)
+
+    #FOR TUTORS
+    #if they didn't specify which classes they will tutor, and the time they will tutor until:
+            #make them specify that(they will type in both)
+    #else
+            #show them how many students need help for each of the classes they are signed up for, and show a button for each class to say help a student from that class
+
     return 'Welcome to Halpzme'
 
-@app.route('/tutorhome/')
+@app.route('/tutor/', methods=['POST'])
 def tutorhome():
-	#if they didn't specify which classes they will tutor, and the time they will tutor until:
-		#make them specify that(they will type in both)
-	#else
-		#show them how many students need help for each of the classes they are signed up for, and show a button for each class to say help a student from that class
-	pass
+    #changes session to tutor
+    pass
 
-@app.route('/studenthome/')
-#look at zach's UI for this
+@app.route('/student/', methods=[ 'POST'])
 def studenthome():
-	#select class they need help for(they will just type in the number)
-		#goto helpwith
-	#show everyone on the board right now, the tutors tutoring their class right now, maybe an ETA
-	#if they not asked for help
-		#show an ask for help button with autofill name and location field based on Ian's API
-	#else
-		#show their place in the queue, ie 4th on the list(should update in real time)
+    #changes session to student
+    pass
 
-	pass
-
-@app.route('/with/<classid>')
+@app.route('/with/<classid>', methods=[ 'POST'])
 def helpwith(classid):
-	pass
+    #student requests tutor for class
+    pass
 
+@app.route('/helped/<studentid>/with/<classid>', methods=[ 'POST'])
+def helped(student,classid):
+    #adds student to queue
+    pass
+
+@app.route('/helpnext/<classid>', methods=[ 'POST'])
+def helpnext(classid):
+    #helps next student in classid
+    pass
+
+@app.route('/cannothelp/<studentid>/with/<classid>', methods=[ 'POST'])
+def cannothelp(student,classid):
+    #puts student back on beginning of queue
+    pass
+ 
+@app.route('/queue/<classid>.json', methods=[ 'GET'])
+def queue(classid):
+    #gets queue for class in json
+    pass   
+
+@app.route('/index/<studentid>/in/<classid>', methods=[ 'POST'])
+def indexedstudent(student,classid):
+    #gets index of student
+    pass
+
+ 
+    
 #achievement idea: over 1 million served(like mcdonalds)
 if __name__ == "__main__":
     app.run()
