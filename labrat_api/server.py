09406083b39562c2134ba2be648b430b09261de5
Alex Tsang <me@a13xt5ang.com>
2013-04-13 04:39:23 -0700

    placeholder api

--- labrat_api/server.py
+++ labrat_api/server.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 from flask import Flask
+from model import PgsqlModel
 import json
 
-
 app = Flask(__name__)
+mod = PgsqlModel()
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Welcome to our api"
+
+@app.route("/location.json", methods=['GET'])
+def location():
+    return json.dump(getLocationFromHost(request.form['host']))
+
+@app.route("/user.json", methods=['GET'])
+def user():
+    return json.dump(getUserFromHost(request.form['host']))
 
 if __name__ == "__main__":
     app.run()
