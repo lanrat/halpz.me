da5cd7422bf3ab1bd5012c445aaecf06c3240e5a
Devin <devin@devin-VirtualBox.(none)>
2013-04-12 20:55:13 -0700

    added flask fucntionality

--- server/model.py
+++ server/model.py
@@ -2,15 +2,18 @@ import redis
 
 class RedisModel(object):
     
-    def __init__(self):
-        self.r = redis.StrictRedis(host='localhost',port=9000,db=0)
+    def __init__(self,host='localhost',port=9000):
+        self.r = redis.StrictRedis(host=host,port=port,db=0)
         
     def studentAdd(self,classid,studentinfo):
-        self.r.ladd(classid,studentinfo)
+        self.r.ladd('queue:'+str(classid),studentinfo)
         
     def getClassSize(self,classid):
-        return self.r.llen(classid)
+        return self.r.llen('queue:'+str(classid))
         
     def popStudent(self,classid):
-        return self.r.rpop(classid)
+        studentinfo = self.r.rpop('queue:'+str(classid))
+        self.r.sadd('pending',studentinfo)
+        return studentinfo
+    
 
