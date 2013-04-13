f0461be0f9209c8451017556f4d094ca79e3578c
Devang Sampat <devangs27@gmail.com>
2013-04-12 21:30:18 -0700

    asdfasdf

--- server/server.py
+++ server/server.py
@@ -19,7 +19,7 @@ def login():
 @app.route('/tutorhome/')
 def tutorhome():
 	#if they didn't specify which classes they will tutor, and the time they will tutor until:
-		#make them specify that
+		#make them specify that(they will type in both)
 	#else
 		#show them how many students need help for each of the classes they are signed up for, and show a button for each class to say help a student from that class
 	pass
@@ -27,7 +27,7 @@ def tutorhome():
 @app.route('/studenthome/')
 #look at zach's UI for this
 def studenthome():
-	#select class they need help for
+	#select class they need help for(they will just type in the number)
 		#goto helpwith
 	#show everyone on the board right now, the tutors tutoring their class right now, maybe an ETA
 	#if they not asked for help
